b7d0aae1f13718d62772ab9ef730b52fdd6fbd1b
Iván Álvarez López <ial.navy@protonmail.ch>
2025-04-19 20:29:39 +0200

    Solve bug 6

--- src/presentation_layer/components/RDFDataMainView.tsx
+++ src/presentation_layer/components/RDFDataMainView.tsx
@@ -42,19 +42,17 @@ let RDFDataMainView: React.FC = () => {
     let [editor, setEditor] = useState<React.ReactNode>(null);
 
     useEffect(() => {
-        if (editor === null) {
-            setEditor(
-                <EditorFactory
-                    code={code}
-                    idDoc={idDoc}
-                    language={getString("mimeTypes.turtle")}
-                    editable={true}
-                    isLineWrapping={isLineWrapping}
-                    fontSize={fontSize}
-                    onChange={(value: string) => { setCode(value); }}
-                />
-            );
-        }
+        setEditor(
+            <EditorFactory
+                code={code}
+                idDoc={idDoc}
+                language={getString("mimeTypes.turtle")}
+                editable={true}
+                isLineWrapping={isLineWrapping}
+                fontSize={fontSize}
+                onChange={(value: string) => { setCode(value); }}
+            />
+        );
     }, [isLineWrapping, fontSize]);
 
     let doFetch = () => {
