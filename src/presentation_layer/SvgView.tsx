12041f09430d2725d68b3e365916d208f85de953
Iván Álvarez López <ial.navy@protonmail.ch>
2025-06-15 21:10:34 +0200

    Unused component removed

--- src/presentation_layer/SvgView.tsx
+++
@@ -1,7 +0,0 @@
-let SvgView: React.FC<{ svgContent: string }> = ({ svgContent }) => {
-    return (
-        <div dangerouslySetInnerHTML={{ __html: svgContent }} />
-    );
-};
-
-export default SvgView;
